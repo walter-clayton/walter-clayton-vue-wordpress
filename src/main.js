a8398e8c7ed1f6cacc0afda98d2ffcb5eb7cc1f9
Walter Clayton <walter.h.clayton@gmail.com>
2024-04-04 01:05:02 +0200

    WC-19

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ const router = createRouter({
   routes: [
       { path: '/', name: 'home', component: () => import('./views/HomeView.vue') },
       { path: '/post/:id', name: 'post', component: () => import('./components/ThePost.vue') },
-      { path: '/about', name: 'about', component: () => import('./views/AboutView.vue') },
     ],
   })
 
